7a6ba619bd73adb2144ab4c08bd8ec607298ee69
Nate Korous <nkorous@managerplus.com>
2020-01-17 11:16:11 -0700

    added toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule, MatButtonModule, MatCardModule, MatIconModule } from '@angular/material'
+import { DxDataGridModule } from 'devextreme-angular'
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    DxDataGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
